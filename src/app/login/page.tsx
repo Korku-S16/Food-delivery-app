53bdb6e682a98001e72e89ce57c2778981cdd705
Korku-S16 <154559759+Korku-S16@users.noreply.github.com>
2024-08-15 16:52:52 +0530

    completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,57 +1,47 @@
-"use client";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import React from "react";
 
-const data = [
-  {
-    id: 1,
-    title: "always fresh & always crispy & always hot",
-    image: "/slide1.png",
-  },
-  {
-    id: 2,
-    title: "we deliver your order wherever you are in NY",
-    image: "/slide2.png",
-  },
-  {
-    id: 3,
-    title: "the best pizza to share with your family",
-    image: "/slide3.jpg",
-  },
-];
-
-const Slider = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(
-      () =>
-        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
-      4000
-    );
-    return () => clearInterval(interval);
-  }, []);
-
+const LoginPage = () => {
   return (
-    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
-      {/* TEXT CONTAINER */}
-      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
-          {data[currentSlide].title}
-        </h1>
-        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
-      </div>
-      {/* IMAGE CONTAINER */}
-      <div className="w-full flex-1 relative">
-        <Image
-          src={data[currentSlide].image}
-          alt=""
-          fill
-          className="object-cover"
-        />
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+      {/* BOX */}
+      <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+        {/* IMAGE CONTAINER */}
+        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+          <Image src="/loginBg.png" alt="" fill className="object-cover"/>
+        </div>
+        {/* FORM CONTAINER */}
+        <div className="p-10 flex flex-col gap-8 md:w-1/2">
+          <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
+          <p>Log into your account or create a new one using social buttons</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+            <Image
+              src="/google.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Google</span>
+          </button>
+          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+            <Image
+              src="/facebook.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Facebook</span>
+          </button>
+          <p className="text-sm">
+            Have a problem?<Link className="underline" href="/"> Contact us</Link>
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Slider;
+export default LoginPage;
